b259856da2e70bdc5e59829dd33a3852851174a6
KaioFranklinDev <kaio.franklin.dev@gmail.com>
2024-06-18 12:46:46 -0300

    ajeitado place holder

--- src/components/CompForm.tsx
+++ src/components/CompForm.tsx
@@ -1,13 +1,14 @@
 interface CompFormProps{
     label:string,
     state:any,
-    funcState:any
+    funcState:any,
+    placeholder:string
 }
 export default function CompForm(props:CompFormProps){
     return(
         <div className="flex flex-col items-center">
             <label>{props.label}</label>
-            <input className="w-1/3 text-center" value={props.state} onChange={(e)=>props.funcState(e.target.value)}/>
+            <input placeholder={props.placeholder} className="w-1/3 text-center" value={props.state} onChange={(e)=>props.funcState(e.target.value)}/>
         </div>
     )
 }
